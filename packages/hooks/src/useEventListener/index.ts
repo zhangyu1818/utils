9530c9eafaaef0887da46864aaea69e953021b87
zhangyu1818 <723156735@qq.com>
2020-10-11 16:59:01 +0800

    feat: useEventListener

---
+++ packages/hooks/src/useEventListener/index.ts
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+import eventListener, { ListenerType } from 'zyutils/es/event-listener';
+
+function useEventListener<T extends keyof WindowEventMap>(
+  target: Window,
+  type: T,
+  listener: (this: Window, ev: WindowEventMap[T]) => any,
+  keys?: [string, string],
+): void;
+function useEventListener<T extends keyof DocumentEventMap>(
+  target: Document,
+  type: T,
+  listener: (this: Document, ev: DocumentEventMap[T]) => any,
+  keys?: [string, string],
+): void;
+function useEventListener<T = any>(
+  target: any,
+  type: string,
+  listener: ListenerType<T>,
+  keys?: [string, string],
+): void;
+function useEventListener<T = any>(
+  target: any,
+  type: string,
+  listener: ListenerType<T>,
+  keys?: [string, string],
+) {
+  React.useEffect(() => eventListener(target, type, listener, keys));
+}
+
+export default useEventListener;
