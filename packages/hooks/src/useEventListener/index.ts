f74d967e2ca97be203bd4bc088c786d7f6fc6f96
zhangyu1818 <723156735@qq.com>
2020-10-13 16:51:31 +0800

    feat: support options

--- packages/hooks/src/useEventListener/index.ts
+++ packages/hooks/src/useEventListener/index.ts
@@ -6,24 +6,26 @@ function useEventListener<T extends keyof WindowEventMap>(
   target: Window,
   type: T,
   listener: (this: Window, ev: WindowEventMap[T]) => any,
-  keys?: [string, string],
+  options?: boolean | AddEventListenerOptions,
 ): void;
 function useEventListener<T extends keyof DocumentEventMap>(
   target: Document,
   type: T,
   listener: (this: Document, ev: DocumentEventMap[T]) => any,
+  options?: boolean | AddEventListenerOptions,
+): void;
+function useEventListener<T = any>(
+  target: any,
+  type: string,
+  listener: ListenerType<T>,
+  options?: unknown,
   keys?: [string, string],
 ): void;
 function useEventListener<T = any>(
   target: any,
   type: string,
   listener: ListenerType<T>,
-  keys?: [string, string],
-): void;
-function useEventListener<T = any>(
-  target: any,
-  type: string,
-  listener: ListenerType<T>,
+  options?: unknown,
   keys?: [string, string],
 ) {
   React.useEffect(() => eventListener(target, type, listener, keys), []);
