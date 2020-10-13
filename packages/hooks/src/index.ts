ea24ce2ff6e0aff0455fc16a4064241522e22a42
zhangyu1818 <723156735@qq.com>
2020-10-13 11:45:05 +0800

    export hooks

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,9 +1,15 @@
 export { default as useClipboard } from './useClipboard';
 export { default as useCssVar } from './useCssVar';
 export { default as useEventListener } from './useEventListener';
+export { default as useFullscreen } from './useFullscreen';
 export { default as useInterval } from './useInterval';
 export { default as useMediaQuery } from './useMediaQuery';
+export { default as usePermission } from './usePermission';
 export { default as usePreferredColorScheme } from './usePreferredColorScheme';
+export { default as usePreferredLanguage } from './usePreferredLanguage';
 export { default as useReactiveRef } from './useReactiveRef';
 export { default as useRefresh } from './useRefresh';
+export { default as useShare } from './useShare';
+export { default as useStartTyping } from './useStartTyping';
 export { default as useTimeout } from './useTimeout';
+export { default as useTitle } from './useTitle';
