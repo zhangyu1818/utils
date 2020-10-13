2cbea3687984730500ad3f9c4f355815fa0071f4
zhangyu1818 <723156735@qq.com>
2020-10-13 16:52:03 +0800

    chore: export hooks

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,9 +1,13 @@
 export { default as useClipboard } from './useClipboard';
 export { default as useCssVar } from './useCssVar';
+export { default as useDocumentVisibility } from './useDocumentVisibility';
 export { default as useEventListener } from './useEventListener';
 export { default as useFullscreen } from './useFullscreen';
+export { default as useGeolocation } from './useGeolocation';
 export { default as useInterval } from './useInterval';
 export { default as useMediaQuery } from './useMediaQuery';
+export { default as useMount } from './useMount';
+export { default as useNetwork } from './useNetwork';
 export { default as usePermission } from './usePermission';
 export { default as usePreferredColorScheme } from './usePreferredColorScheme';
 export { default as usePreferredLanguage } from './usePreferredLanguage';
