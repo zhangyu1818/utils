6e8745c86babb92c0a6ed1324fac1780192ab857
zhangyu1818 <723156735@qq.com>
2020-10-13 16:51:10 +0800

    chore: eslint warning

--- packages/utils/src/dev-warning.ts
+++ packages/utils/src/dev-warning.ts
@@ -1,5 +1,6 @@
 const devWarning = (assert: any, warning: string) => {
   if (process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line no-console
     if (assert) console.warn(warning);
   }
 };
