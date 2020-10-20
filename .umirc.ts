5554c5bd4852b6bcc06a0ef40e18570908c705a0
zhangyu1818 <723156735@qq.com>
2020-10-20 20:14:10 +0800

    docs deploy

--- .umirc.ts
+++ .umirc.ts
@@ -1,3 +1,9 @@
 import { defineConfig } from 'dumi';
 
-export default defineConfig({});
+export default defineConfig({
+  title: 'utils',
+  outputPath: 'docs-dist',
+  publicPath: 'https://zhangyu1818.github.io/utils/',
+  base: '/utils/',
+  exportStatic: {},
+});
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-utils
+lerna管理的React hooks和utils工具函数
+
+```sh
+yarn add yuhooks
+yarn add yuutils
+```
+
+[文档](https://zhangyu1818.github.io/utils/)
